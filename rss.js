5356473855e301a88b7f3e2255beeb58f4f3b6a8
Arjun Krishna <arjunkrishna@gmail.com>
2020-02-21 21:24:50 -0500

    added Show Description

--- rss.js
+++ rss.js
@@ -1,5 +1,3 @@
-
-
 var urlParam = function(name, w) {
   w = w || window;
   var rx = new RegExp("[&|?]" + name + "=([^&#]+)"),
@@ -7,55 +5,54 @@ var urlParam = function(name, w) {
   return !val ? "" : val[1];
 };
 
-
-function empty(data)
-{
-  if(typeof(data) == 'number' || typeof(data) == 'boolean')
-  {
+function empty(data) {
+  if (typeof data == "number" || typeof data == "boolean") {
     return false;
   }
-  if(typeof(data) == 'undefined' || data === null)
-  {
+  if (typeof data == "undefined" || data === null) {
     return true;
   }
-  if(typeof(data.length) != 'undefined')
-  {
+  if (typeof data.length != "undefined") {
     return data.length == 0;
   }
   var count = 0;
-  for(var i in data)
-  {
-    if(data.hasOwnProperty(i))
-    {
-      count ++;
+  for (var i in data) {
+    if (data.hasOwnProperty(i)) {
+      count++;
     }
   }
   return count == 0;
 }
 
-function getRssFeed(Url, MaxCount, ContentLimit)
-{  
-  $('#divRss').FeedEk({
-    FeedUrl:Url,
-    MaxCount : MaxCount,
-    ShowDesc : true,
-    ShowPubDate:false,
-    DescCharacterLimit:ContentLimit,
-    TitleLinkTarget:'_blank',        
-    });
+function getBool(p) {
+  if (p == 1) return true;
+  else return false;
+}
+
+function getRssFeed(Url, MaxCount, ContentLimit, ShowDesc) {
+  $("#divRss").FeedEk({
+    FeedUrl: Url,
+    MaxCount: MaxCount,
+    ShowDesc: getBool(ShowDesc),
+    ShowPubDate: false,
+    DescCharacterLimit: ContentLimit,
+    TitleLinkTarget: "_blank"
+  });
 }
 
 var RssFeedUrl = urlParam("RssFeedUrl");
 var MaxCount = urlParam("MaxCount");
 var ContentLimit = urlParam("ContentLimit");
-if (empty(MaxCount)) {  
-  MaxCount = 5
+var ShowDesc = urlParam("ShowDesc");
+if (empty(MaxCount)) {
+  MaxCount = 5;
 }
 if (empty(ContentLimit)) {
-  ContentLimit = 100
+  ContentLimit = 100;
+} else if (ContentLimit > 1000) {
+  ContentLimit = 1000;
 }
-else if (ContentLimit > 1000)
-{
-  ContentLimit = 1000
+if (empty(ShowDesc)) {
+  ShowDesc = 0;
 }
-getRssFeed(RssFeedUrl, MaxCount, ContentLimit);
+getRssFeed(RssFeedUrl, MaxCount, ContentLimit, ShowDesc);
